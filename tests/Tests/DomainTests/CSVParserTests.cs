68a0f80effcd86dfa01f0d3c9b77ebc025c8911e
adammenkiel <publicprojectsmenkiel@gmail.com>
2026-07-25 18:54:32 +0200

    comma unit test

--- tests/Tests/DomainTests/CSVParserTests.cs
+++ tests/Tests/DomainTests/CSVParserTests.cs
@@ -16,4 +16,23 @@ public class CSVParserTests
         //Assert
         Assert.Equal(JsonSerializer.Serialize(info.JsonContent), ExceptedResult);        
     }
+
+    /*
+        Test checks if injecting " in CSV syntax is possible,
+        if yes this may be broke JSON or even cause another JSON injection
+        inside another JSON althought it seem not very probably because of
+        comma isn't able to write at CSV name
+    */
+    [Fact]
+    public void CSVInjectionTest()
+    {
+        //Arrange
+        CSVParser sut = new();
+        string input = "a,b,c\n1,2,3\"\n4,5,6\n7,8,9";
+
+        //Act & Assert
+        Assert.Throws<ParseException>(() => {
+            sut.parse(input);
+        });
+    }
 }
